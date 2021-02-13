95cef736ac586dc4bfc43be30f86f32fefc2eb0e
NON_A <alsdn202530@gmail.com>
2021-02-14 03:24:20 +0900

    It Works!

--- src/TestringGit/LetsTestGit.java
+++ src/TestringGit/LetsTestGit.java
@@ -2,5 +2,5 @@ package TestringGit;
 
 public class LetsTestGit {
     private final String im = "Testing Git Now..";
-    private String and ="aaa";
+    private String and ="Typing always FeelssoososoGood";
 }
